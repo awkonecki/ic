b141985de2a3cb4db7c842749e75bf30d5346b7f
awkonecki <awkonecki.developer@gmail.com>
2018-09-04 12:40:11 -0700

    [FTR] Challenge 6 - Find Rotation Index of Sorted List
    Feature Description
    
    Serial and recursive implementations for finding the index of the
    rotation of a sorted list.
    
    Related Issues
    - #12

--- src/main/java/challenge-6/README.md
+++ src/main/java/challenge-6/README.md
@@ -14,3 +14,7 @@ Given an array of sorted strings that have been rotated determine the index in w
 - Can implement a classic binary search.  
 - Conditions for the rotation is that the words to the left are greater
 - words to the right are greater
+
+## Problem Issues
+- The converging point for the solution is a bit odd, and is typically not how I normally implement binary search.
+- The converge technique is being used for the base case definition.
--- src/main/java/challenge-6/Solution.java
+++ src/main/java/challenge-6/Solution.java
@@ -1,40 +1,63 @@
 public class Solution {
 
-    public static int [] function(int [] nums) {
-        if (nums == null || nums.length < 2) {
-            return new int [0];
+    public static int function(String [] words) {
+        if (words[0].compareTo(words[words.length - 1]) <= 0) {
+            return 0;
+        }
+        assert (recursive(words, 0, words.length - 1) == serial(words));
+        return serial(words);    
+    }
+
+    private static int serial(String [] words) {
+        int lowIndex = 0;
+        int highIndex = words.length - 1;
+
+        while (lowIndex < highIndex) {
+
+            // guess a point halfway between floor and ceiling
+            int index = (highIndex - lowIndex) / 2 + lowIndex;
+
+            // if guess comes after first word or is the first word
+            if (words[index].compareTo(words[0]) >= 0) {
+                // go right
+                lowIndex = index + 1;
+            } else {
+                // go left
+                highIndex = index;
+            }
         }
 
-        int [] result = new int [nums.length];
-        int forward = 1, backward = 1;
+        return highIndex;
+    }
 
-        for (int i = 1; i < nums.length; i++) {
-            forward *= nums[i - 1];
-            result[i] = forward;
+    private static int recursive(String [] words, int lowIndex, int highIndex) {
+        if (lowIndex < highIndex) {
+            int index = (highIndex - lowIndex) / 2 + lowIndex;
+            String word = words[index];
+
+            if (words[0].compareTo(word) > 0) {
+                highIndex = index;
+            }
+            else {
+                // go right
+                lowIndex = index + 1;
+            }
+
+            return recursive(words, lowIndex, highIndex);
         }
-
-        for (int i = nums.length - 2; i > 0; i--) {
-            backward *= nums[i + 1];
-            result[i] *= backward;
+        else if (lowIndex == highIndex) {
+            return highIndex;
         }
-
-        result[0] = backward;
-        result[result.length - 1] = forward;
-
-        return result;
+        else {
+            return -1;
+        }
     }
     
     public static void main(String [] args) {
-        assert (function(null).length == 0);
-        assert (function(new int [0]).length == 0);
-        assert (function(new int [1]).length == 0);
-
-        int [] singleZero = new int [] {0,1,2,3,4,5};
-        int [] singleZeroRsult = new int [] {120,0,0,0,0,0};
-        int index = 0;
-        for (int num : function(singleZero)) {
-            assert (singleZeroRsult[index] == num);
-            index++;
-        }
+        assert (function(new String[] {"a","b","c","d","e"}) == 0);
+        assert (function(new String[] {"e","a","b","c","d"}) == 1);
+        assert (function(new String[] {"d","e","a","b","c"}) == 2);
+        assert (function(new String[] {"c","d","e","a","b"}) == 3);
+        assert (function(new String[] {"b","c","d","e","a"}) == 4);
     }
 }
