8bf08815c81552857abff8ffd20eba0afc4e8aab
awkonecki <awkonecki.developer@gmail.com>
2018-10-05 19:45:17 -0700

    [FTR] Challenge 9 - [A]nalysis Cake Thief
    Feature Description
    
    Provided the challenge analysis to further investigate the case for
    dynamic programming.
    
    Related Issues
    - #19

--- src/main/java/challenge-9/Solution.java
+++ src/main/java/challenge-9/Solution.java
@@ -19,10 +19,14 @@ public class Solution {
 
         int cakeWeight = cakes[cakeIndex][0];
         int cakeValue = cakes[cakeIndex][1];
-        int cakeCount = 0;
+        int cakeCount = 1;
         int max = maxValue;
 
-        while (cakeCount * cakeWeight <= capacityRemaining) {
+        // excluded
+        max = getMaxStolenValue(cakes, cakeIndex + 1, capacityRemaining, currentValue, max); 
+
+        // Included
+        while (cakeCount * cakeWeight <= capacityRemaining && cakeCount * (cakeValue + currentValue) > currentValue) {
             max = getMaxStolenValue(cakes, cakeIndex + 1, capacityRemaining - (cakeCount * cakeWeight), currentValue + (cakeValue * cakeCount), max); 
             cakeCount++;   
         }
@@ -30,13 +34,29 @@ public class Solution {
         return max;
     }
 
+    // [A]nalysis
+    // 1. Does the problem have optimal sub-structure?
+    // Yes each level is independent of the others.
+    // 2. Does the problem have re-occurring problems?
+    // Yes, there is a possibility based on the data that the same max value, 
+    // current value and index are presented.
+
+    // Performance
+    // Memory 
+    // O(N) - where N is the number of cakes
+
+    // Time
+    // O(D^N) where D is the capacity / minimum weight.
+
     public static int getMaxStolenValue(int [][] cakes, int capacity) {
         return getMaxStolenValue(cakes, 0, capacity, 0, 0);
     }
 
     public static void main(String [] args) {
         System.out.println(getMaxStolenValue(new int [][] {
-            {2, 1}
+            {2, 1},
+            {2, 0},
+            {0, 10000}
         }, 9));
     }
 }
